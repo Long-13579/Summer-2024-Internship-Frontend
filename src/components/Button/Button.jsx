59aa0936cc42161bdf65106990e7b6745504188a
Nguyễn Quốc Toàn <108344477+TonNQ@users.noreply.github.com>
2024-06-24 13:58:05 +0700

    feat: create header component (#3)
    setup tailwindcss and shadcn-ui, create header component for user pages
    
    86epmehnt

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,24 @@
-export default function Button() {
-  return <div>Button</div>
+import { cn } from '@/lib/utils'
+
+export default function Button({ title, icon, extraClass = '', imgClass = '', txtClass = '', ...rest }) {
+  return (
+    <div
+      className={cn(
+        'group relative flex items-center gap-2 overflow-hidden rounded-md bg-yellow-custom-700 px-4 py-2 text-black transition-colors duration-1000 ease-in-out hover:cursor-pointer hover:text-white',
+        extraClass
+      )}
+      {...rest}
+    >
+      {icon && (
+        <img
+          src={icon.src}
+          alt={icon.alt}
+          className={cn('transition-filter z-10 h-7 duration-500 ease-in-out group-hover:invert', imgClass)}
+        />
+      )}
+      <span className={cn('ml-sm z-10 text-lg font-bold uppercase group-hover:text-white', txtClass)}>{title}</span>
+      <span className='z-1 absolute inset-0 rounded-md bg-yellow-custom-700 transition-transform duration-500 ease-in-out group-hover:translate-x-0'></span>
+      <span className='z-2 absolute inset-0 -translate-x-full transform rounded-md bg-gradient-to-r from-purple-custom-700 to-blue-custom-700 transition-transform duration-500 ease-in-out group-hover:translate-x-0'></span>
+    </div>
+  )
 }
--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -5,7 +5,7 @@ export default function MainLayout() {
   return (
     <>
       <HeaderUser />
-      <div className='container'>
+      <div className='mx-auto my-0 block max-w-container pt-header-user'>
         <Outlet />
       </div>
     </>
