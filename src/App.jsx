6f1d1270bf71a879040049cba38842af608e2898
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-07-26 10:28:38 +0700

    chore: scroll to the top of page when navigate
    Scroll to the top of page when navigate
    
    86epxcjnz

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { ToastContainer } from 'react-toastify'
 import useRouteElements from './routes/useRouteElements'
 import 'react-toastify/dist/ReactToastify.css'
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 function App() {
   const routeElements = useRouteElements()
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [pathname])
+
   return (
     <>
       {routeElements}
